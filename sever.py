56c9f986f80f03b425cd57ab5c450d419c570fcc
cygwong <gwong98@gmail.com>
2022-03-27 17:33:07 -0700

    added delete function

--- sever.py
+++ sever.py
@@ -78,8 +78,23 @@ def edit_post(post_id):
     author=post.author,
     body=post.body
     )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+
     return render_template("make-post.html", form=edit_form, is_edit=True)
 
+@app.route('/delete/<int:post_id>')
+def delete_post(post_id):
+    post = BlogPost.query.get(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
 
 @app.route("/about")
 def about():
